cba1e0b322b0ad3e4c2fff7a3495497d6bfd1f11
Sergio Restrepo Prado <153745435+SergioRP18@users.noreply.github.com>
2025-02-20 18:43:24 -0500

    Add files via upload

---
+++ Proyecto/Coursera/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './components/Card/Card'
+import Nav from './components/Navbar/Nav'
+
+function App() {
+
+  return (
+    <>
+      <Nav></Nav>
+    </>
+  )
+}
+
+export default App
---
+++ Proyecto/Coursera/src/components/Card/Card.jsx
@@ -0,0 +1,7 @@
+function Card () {
+    return(
+        <h1>Hola</h1>
+    )
+}
+
+export default Card
